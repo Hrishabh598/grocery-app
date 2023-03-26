1bf81799d9fe96d93bd4a6b82d3b4de2aba40200
hrishabh598 <hrishabhtiwari598@gmail.com>
2023-03-27 02:52:10 +0530

    import react-dom eroor lol

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react'
 
 const App = () => {
-  return (<div><p>App</p></div>);
+  return (
+  <div>
+    <p>App</p>
+    <h1>This is some heading</h1>
+    </div>
+  );
 }
 
 export default App;
